c1e9a3606ade3b69ca9cfe7698a7a3e15987ae9a
Alejandro Vivas <alejandrojv17@gmail.com>
2018-10-10 10:00:14 -0300

    fix service

--- assets/services/users.service.js
+++ assets/services/users.service.js
@@ -1,31 +1,3 @@
-/* let users = [
-    {
-        id: 1,
-        name:'Alejandro',
-        isActive: 1
-    },
-    {
-        id: 2,
-        name:'Jose',
-        isActive: 1
-    },
-    {
-        id: 3,
-        name:'Daniel',
-        isActive: 0
-    },
-    {
-        id: 4,
-        name:'Luis',
-        isActive: 1
-    }
-]; 
-
-module.exports = users
-
-
-*/
-
 import axios from 'axios';
 
 const RESOURCE_NAME = "/users";
